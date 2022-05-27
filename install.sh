60f9801980cd8b9574ab6564b8b0bb9af06606f6
Jerry <91048755+Jerry4539@users.noreply.github.com>
2022-05-27 08:15:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
-echo -e $b">"$w" SARA - Simple android ransomware attack"
+echo -e $b">"$w" Ransome - Simple android ransomware attack"
 echo -e $b">"$w" prepare for installing dependencies ..."
 sleep 3
 echo -e $b">"$w" installing package: "$g"default-jdk"$w
@@ -20,4 +20,4 @@ sudo apt-get install python3 python3-pip -y
 echo -e $b">"$w" installing modules: "$g"pillow"$w
 pip3 install Pillow
 echo -e $b">"$w" successfully installing dependencies"
-echo -e $b">"$w" use command "$g"python3 sara.py"$w" for start the console"
+echo -e $b">"$w" use command "$g"python3 ransome.py"$w" for start the console"
